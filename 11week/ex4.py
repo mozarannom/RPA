80a1e15ba34f05c84c3c23ed0f4a8c3624833714
mozarannom <parkyeanwoo8@gmail.com>
2024-11-21 16:36:42 +0900

    11주차 실습파일

---
+++ 11week/ex4.py
@@ -0,0 +1,11 @@
+import qrcode
+
+name = input("이름을 입력하시오 : ")
+code = input("학번을 입력하시오 : ")
+major = input("전공을 입력하시오 : ")
+
+qr_data= f"{name},{code},{major}"
+qr_img = qrcode.make(qr_data)
+
+save_path = 'my_info_data.png'
+qr_img.save(save_path)
